073df78459e65f01473815dc4b22ca90df5e83cb
Bryan Mills <bryan@Bryans-MacBook-Pro.local>
2015-09-17 10:24:05 -0400

    Coverted library to stright C

--- hw3/io.h
+++ hw3/io.h
@@ -2,34 +2,32 @@
  * University of Pittsburgh
  * Department of Computer Science
  * CS1645: Introduction to HPC Systems
- * Instructor Bryan Mills, PhD
- * Original Code Esteban Meneses
+ * Instructor Bryan Mills, PhD 
+ * Original Code Esteban Meneses (converted to c)
  * Input/output operations for matrices.
  */
 
-#include <iostream>
-#include <fstream>
-
-using namespace std;
+#include<stdio.h>
 
 // Reads a matrix from text file
-int readMatrixFile(int **matrix, int N, char *fileName){
-  ifstream file(fileName);
-  if(file.is_open()){
-    
-    // reading matrix values
-    for(int i=0; i<N; i++){
-      for(int j=0; j<N; j++){
-	file >> matrix[i][j];
+int readMatrixFile(int **matrix, int N, char *filename){
+
+  FILE *ifp, *ofp;
+  char *mode = "r";
+  ifp = fopen(filename, mode);
+
+  if (ifp == NULL) {
+    fprintf(stderr, "Can't open input file in.list!\n");
+    return 1;
+  }
+
+  for (int i=0; i<N; i++) {
+    for (int j=0; j<N; j++) {
+      if (!fscanf(ifp, "%d", &matrix[i][j])) {
+	fprintf(stderr, "Unable to read!\n");
+	return 1;
       }
     }
-
-    // closing file
-    file.close();
-
-  } else {
-    cout << "Error opening file: " << fileName << endl;
-    return 1;
   }
   return 0;
 }
@@ -38,8 +36,22 @@ int readMatrixFile(int **matrix, int N, char *fileName){
 void printMatrix(int **matrix, int N){
   for(int i=0; i<N; i++){
     for(int j=0; j<N; j++){
-      cout << matrix[i][j] << "\t";
+      printf("%d\t", matrix[i][j]);
     }
-    cout << endl;
+    printf("\n");
   }
+}
+
+// Compares the two matrixes and returns the number of cells which are
+// not equal.
+int compareMatrix(int **M1, int **M2, int N) {
+  int c = 0;
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      if (M1[i][j] != M2[i][j]) {
+	c++;
+      }
+    }
+  }
+  return c;
 }
--- hw3/timer.h
+++ hw3/timer.h
@@ -3,24 +3,23 @@
  * Department of Computer Science
  * CS1645: Introduction to HPC Systems
  * Instructor Bryan Mills, PhD
- * Original Code from Esteban Meneses
+ * Original Code from Esteban Meneses (coverted to C)
  * Timing operations.
  */
 
-#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-double startTime;
+clock_t start, diff;
 
 // Starts timer and resets the elapsed time
 void timerStart(){
-  struct timeval tod;
-  gettimeofday(&tod, NULL);
-  startTime = (double)tod.tv_sec + ((double)tod.tv_usec * 1.0e-6); 
+  start = clock();
 }
 
-// Stops the timer and returns the elapsed time
-double timerStop(){
-  struct timeval tod;
-  gettimeofday(&tod, NULL);
-  return ((double)tod.tv_sec + ((double)tod.tv_usec * 1.0e-6)) - startTime;
+// Stops the timer and returns elapsed time in msec
+long timerStop(){
+  diff = clock() - start;
+  return (diff * 1000) / CLOCKS_PER_SEC;
 }

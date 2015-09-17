391f2b1826954008f9e46746c1e49df83b5f5c7f
Bryan Mills <bryan@Bryans-MacBook-Pro.local>
2015-09-17 10:22:11 -0400

    Migrated everything to straight c

---
+++ hw3/strassen.c
@@ -0,0 +1,124 @@
+/**
+ * University of Pittsburgh
+ * Department of Computer Science
+ * CS1645: Introduction to HPC Systems
+ * Instructor Bryan Mills, PhD
+ * Student: 
+ * Implement Pthreads version of Strassen algorithm for matrix multiplication.
+ */
+
+#include "timer.h"
+#include "io.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Make these globals so threads can operate on them. You will need to
+// add additional matrixes for all the M and C values in the Strassen
+// algorithms.
+int **A;
+int **B;
+int **C;
+// Reference matrix, call simpleMM to populate.
+int **R;
+
+// Stupid simple Matrix Multiplication, meant as example.
+void simpleMM(int N) {
+  for (int i=0; i<N; i++) {
+    for (int j=0; j<N; j++) {
+      for (int k=0; k<N; k++) {
+	R[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+}
+
+// WRITE YOUR CODE HERE, you will need to also add functions for each
+// of the sub-matrixes you will need to calculate but you can create your
+// threads in this fucntion.
+void strassenMM(int N) {
+  
+}
+
+// Allocate square matrix.
+int **allocMatrix(int size) {
+  int **matrix;
+  matrix = (int **)malloc(size * sizeof(int *));
+  for (int row = 0; row < size; row++) {
+    matrix[row] = (int *)malloc(size * sizeof(int));
+  }
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      matrix[i][j] = 0;
+    }
+  }
+  return matrix;
+}
+
+// Allocate memory for all the matrixes, you will need to add code
+// here to initialize any matrixes that you need.
+void initMatrixes(int N) {
+  A = allocMatrix(N); B = allocMatrix(N); C = allocMatrix(N); R = allocMatrix(N);
+}
+
+// Free up matrixes.
+void cleanup() {
+  free(A);
+  free(B);
+  free(C);
+  free(R);
+}
+
+// Main method
+int main(int argc, char* argv[]) {
+  int N;
+  double elapsedTime;
+
+  // checking parameters
+  if (argc != 2 && argc != 4) {
+    printf("Parameters: <N> [<fileA> <fileB>]\n");
+    return 1;
+  }
+  N = atoi(argv[1]);
+  initMatrixes(N);
+
+  // reading files (optional)
+  if(argc == 4){
+    readMatrixFile(A,N,argv[2]);
+    readMatrixFile(B,N,argv[3]);
+  } else {
+    // Otherwise, generate two random matrix.
+    for (int i=0; i<N; i++) {
+      for (int j=0; j<N; j++) {
+	A[i][j] = rand() % 5;
+	B[i][j] = rand() % 5;
+      }
+    }
+  }
+
+  // Do simple multiplication and time it.
+  timerStart();
+  simpleMM(N);
+  printf("Simple MM took %ld ms\n", timerStop());
+
+  // Do strassen multiplication and time it.
+  timerStart();
+  strassenMM(N);
+  printf("Strassen MM took %ld ms\n", timerStop());
+
+  if (compareMatrix(C, R, N) != 0) {
+    if (N < 20) {
+      printf("\n\n------- MATRIX C\n");
+      printMatrix(C,N);
+      printf("\n------- MATRIX R\n");
+      printMatrix(R,N);
+    }
+    printf("Matrix C doesn't match Matrix R, if N < 20 they will be printed above.\n");
+  }
+
+  // stopping timer
+  
+  cleanup();
+  return 0;
+}

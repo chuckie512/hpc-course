2b5ec790be826fc8b42ee905499d1b0a2c31aed9
Charles Smith <cas275@pitt.edu>
2015-11-10 01:01:16 -0500

    much easier way to do what i just did

--- hw7/particles.c
+++ hw7/particles.c
@@ -93,11 +93,7 @@ main(int argc, char** argv){
       globals = (struct Particle *) malloc(n * sizeof(struct Particle));
 
       // YOUR CODE GOES HERE (reading particles from file)
-      for(int i=0; i<size; i++){
-        fscanf(ifp, "%f\t%f\t%f", &globals[i].x, &globals[i].y, &globals[i].mass);
-        globals[i].fx = 0.0;
-        globals[i].fy = 0.0;
-      }
+      read_file(globals,n,argv[2]);
       // END MY CODE
     }
     
